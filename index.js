4517ee203eec1e3c2a6ca5bc5d5e8ab2650b7fff
Paul Lurin <paul.lurin@gmail.com>
2021-02-05 21:51:52 +0100

    premier push

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+  const api = require("api-ecoledirecte-france")
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+api.accounts("LeBonMoustique", "Lurin92")
+    .then((accounts) => {
+        const prenom = accounts[0].prenom;
+        const nom = accounts[0].nom;
+        const fam = prenom + " " + nom;
+        let file = fs.readFileSync('./index.html', {encoding: 'utf8'})
+        
+        console.log(accounts);
+
+        file = file.replace('{{NAME}}',fam);
+        
+
+        res.write(file);
+        res.end();
+
+        
+    })
+    .catch((err) => {
+        throw err
+    })
+ });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+/* ------------------------------------------------------------------------ */
