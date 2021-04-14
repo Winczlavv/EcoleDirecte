31d685a35c7b1bd046a79c7f17076495d9cdc926
Matteo Bonnet <matteobonnet@macbook-pro.home>
2021-04-14 23:27:16 +0200

    session variable + fetch notes/infos user

--- index.js
+++ index.js
@@ -3,36 +3,76 @@ const http = require('http');
 require('dotenv').config();
 const port = 3000;
 const EcoleDirecte = require("node-ecole-directe");
-const session = new EcoleDirecte.Session();
+const ecole = new EcoleDirecte.Session();
 const express = require('express')
-const app = express()
-
-// Create a server
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-});
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var app = express()
 
 
-async function getNotes(){
-  const compte = await session.connexion(process.env.LOGIN, process.env.PASSWORD);
+
+// EJS
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+// Middleweres
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+
+
+// Functions
+async function getNotes(username, mdp){
+  const compte = await ecole.connexion(username, mdp);
   const notes = await compte.fetchNotes();
   return notes;
 }
 
-async function getEmploiDuTemps(){
-  const compte = await session.connexion(process.env.LOGIN, process.env.PASSWORD);
+async function getEmploiDuTemps(username, mdp){
+  const compte = await ecole.connexion(username, mdp);
   const emploiDuTemps = await compte.fetchEmploiDuTemps();
   return emploiDuTemps;
 }
 
+
+
 // Roots
-app.get('/', async (req, res) => {
-  const emploiDuTemps = await getEmploiDuTemps();
-  console.log(emploiDuTemps);
-  res.end();
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/home', async (req, res) => {
+  var username = req.body.nom;
+  var mdp = req.body.mdp;
+  try {
+    const compte = await ecole.connexion(username, mdp);
+    var sess = req.session;
+    sess.compte = compte;
+    const notes = await getNotes(username, mdp);
+    var last = notes.notes.length - 1;
+    res.render('home', {username:username, mdp:mdp, lastNote:notes.notes[last]});
+  }
+  catch(err) {
+    res.render('index', {error: "Identifiant ou mot de passe incorrect"});
+  }
+});
+
+
+app.post('/profil', async (req, res) => {
+  console.log(req.session.compte.data.profile.classe.id);
+  res.render('profil', {compte:req.session.compte});
 })
 
+app.post('/notes', async (req, res) => {
+  var username = req.body.username;
+  var mdp = req.body.mdp;
+  const compte = await ecole.connexion(username, mdp);
+  const notes = await getNotes(username, mdp);
+});
 
+
+// Listening
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
